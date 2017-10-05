6343f971d994ff1b9daa5a4e2495d43d419b0845
Peter Hughes <peterhughes.dev@gmail.com>
2017-10-05 11:59:08 -0700

    Add BBCode support

--- start/hooks.js
+++ start/hooks.js
@@ -1,4 +1,24 @@
+global.window = undefined
+
 const { hooks } = require('@adonisjs/ignitor')
+const XBBCode = require('xbbcode')
+
+const bbcodeFilter = XBBCode.create({
+  b: '<strong>{content}</strong>',
+  i: '<em>{content}</em>',
+  u: '<u>{content}</u>',
+  code: '<code>{content}</code>',
+  img: '<img src="{source}" alt="Image({source}" />',
+  size: tag => `<span style="font-size: ${tag.getOption()}%">${tag.getContent()}</span>`,
+  url: tag => `<a href="${tag.getOption()}" target="_blank">${tag.getContent()}</a>`,
+  color: tag => `<span style="color: ${tag.getOption()}">${tag.getContent()}</span>`,
+  table: '<table class="table table-striped">{content}</table>',
+  tr: '<tr>{content}</tr>',
+  td: '<td>{content}</td>',
+  left: '<span style="text-align: left">{content}</span>',
+  center: '<span style="text-align: center">{content}</span>',
+  right: '<span style="text-align: right">{content}</span>'
+})
 
 hooks.after.providersBooted(() => {
   const View = use('View')
@@ -6,5 +26,9 @@ hooks.after.providersBooted(() => {
   View.global('linkTo', function (route, name, params) {
     const routeTo = this.resolve('route')
     return this.safe(`<a href="${routeTo(route, params)}"> ${name} </a>`)
+  })
+
+  View.global('bbcode', function (content) {
+    return this.safe(bbcodeFilter(content).replace(/(?:\r\n|\r|\n)/g, '<br />'))
   })
 })
